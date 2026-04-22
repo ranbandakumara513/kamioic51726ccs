48ba6784f2ba09f82b54653beee81eec775c7527
ranbandakumara513 <ranbandakumara513@gmail.com>
2026-04-22 17:55:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ranu&pi01EbRQ#1JGr9jAiWS3bP5IeTWacZQjjsGiifkrK23Amkj6xFkI",
+SESSION_ID: process.env.SESSION_ID || "ranu&Ej8C0bga#x6goucpiNd-b1kKI2ZLTgz3-lRM0ayfS-y8bxOguBE4",
 // add your Session Id (මුලට "ranu&")
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
 // make it true for auto recoding 
